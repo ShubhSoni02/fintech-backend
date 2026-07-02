5bd01f681355e3ca70d02f357587734e3257f0a9
ShubhSoni <shubhsoni270@gmail.com>
2026-07-02 16:55:02 +0530

    added sample cors just for testing

--- app/main.py
+++ app/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import users, auth, transactions
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = False,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
 
 app.include_router(users.router)
 app.include_router(auth.router)
