e3be49c1eac981f42e564773db72e41d7c20c6f0
ShubhSoni <shubhsoni270@gmail.com>
2026-07-13 13:05:33 +0530

    fixed personal data fatch endpoint

--- app/routers/users.py
+++ app/routers/users.py
@@ -28,15 +28,9 @@ def create_user(user : schemas.UserCreate, db : Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.UserOut)
-def get_user(id : int, db : Session = Depends(get_db), current_user = Depends(oauth2.get_current_user)):
-    if current_user.id != id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized.")
-    
-    user = db.query(models.User).filter(models.User.id == id).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id : {id} not found.")
-    return user
+@router.get("/me", status_code=status.HTTP_200_OK, response_model=schemas.UserOut)
+def get_me(current_user = Depends(oauth2.get_current_user)):   
+    return current_user
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_user(id : int, db : Session = Depends(get_db), current_user = Depends(oauth2.get_current_user)):
